2ae1115c5656b5f74d13a8373a3e205e8f387b98
Ciuca Veronica <veronicaciuca17@gmail.com>
2024-04-20 00:08:13 +0300

    changes

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 const ageForm  = document.getElementById('ageForm');
-const day = document.getElementById('day');
-const month = document.getElementById('month');
-const year = document.getElementById('year');
+const dayInput = document.getElementById('day');
+const monthInput = document.getElementById('month');
+const yearInput = document.getElementById('year');
 const labels = document.querySelectorAll('label');
 const inputs = document.querySelectorAll('input');
 const smalls = document.querySelectorAll('small');
-const errorDay = document.querySelector('.error-day');
-const errorMonth = document.querySelector('.error-month');
-const errorYear = document.querySelector('.error-year');
 const dayOutput = document.getElementById('dayOutput');
 const monthOutput = document.getElementById('monthOutput');
 const yearOutput = document.getElementById('yearOutput');
 
 const today = new Date();
-const dayDate = today.getDate();
-const monthDate = today.getMonth() + 1;
-const yearDate = today.getFullYear();
-let yearAge = '';
+const dayNow = today.getDate();
+const monthNow = today.getMonth() + 1;
+const yearNow = today.getFullYear();
 
 ageForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    if(day.value === '' || month.value === '' || year.value === '') {
+const yearValue = Number(yearInput.value);
+const dayValue = Number(dayInput.value);
+const monthValue = Number(monthInput.value);
+
+    function leapYear(year)
+{
+  return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+}
+
+const result = leapYear(Number(yearValue));
+console.log(result);
+
+    if(dayValue === '' || monthValue === '' || yearValue === '') {
         labels.forEach(label => {
             label.classList.add('error');
         });
@@ -30,12 +38,34 @@ ageForm.addEventListener('submit', (e) => {
        smalls.forEach(small => {
         small.innerText = 'This field is required';
        });
-    } else if(day.value > 31 && month.value > 12 && year.value > yearDate) {
-       labels[0].classList.add = 'error';
-       labels[3].classList.add = 'error';
-        errorDay.innerText = 'Must be a valid day';
-        errorYear.innerText = 'Must be in the past';
-    } else {
+    } else if(dayValue > 31 || monthValue > 12 || yearValue > yearNow) {
+        labels.forEach(label => {
+            label.classList.add('error');
+        });
+        inputs.forEach(input => {
+            input.classList.add('error');
+        });
+       smalls[0].innerText = 'Must be a valid day';
+       smalls[1].innerText = 'Must be a valid month';
+       smalls[2].innerText = 'Must be in the past';
+    } else if(monthValue === 2 && dayValue > 28 && result === false) {
+        labels.forEach(label => {
+            label.classList.add('error');
+        });
+        inputs.forEach(input => {
+            input.classList.add('error');
+        });
+       smalls[0].innerText = 'This month has only 28 days';
+    } else if(dayValue > 30 && monthValue % 2 === 0) {
+        labels.forEach(label => {
+            label.classList.add('error');
+        });
+        inputs.forEach(input => {
+            input.classList.add('error');
+        });
+       smalls[0].innerText = 'This month has only 30 days';
+    } 
+     else {
         labels.forEach(label => {
             label.classList.remove('error');
         });
@@ -45,8 +75,26 @@ ageForm.addEventListener('submit', (e) => {
         });
         smalls.forEach(small => {
             small.innerText = '';
-        });
+        }); 
 
-      
+        let yearAge = 0;
+        yearAge = yearNow - yearValue;
+        yearOutput.innerHTML = yearAge;
+        console.log(yearAge)
+        yearOutput.classList.add('success');
+
+        let monthAge = 0;
+        monthAge = yearAge * 12;
+        monthOutput.innerHTML = monthAge;
+        monthOutput.classList.add('success');
+
+        let daysAge = 0; 
+        daysAge = yearAge * 365;
+        dayOutput.innerHTML = daysAge;
+        dayOutput.classList.add('success');  
     }
-});
+}); 
+
+
+
+
