75827ec7bd164388729a1bcd8879e394a7b4072c
Ciuca Veronica <veronicaciuca17@gmail.com>
2024-04-21 10:09:46 +0300

    modified script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const ageForm  = document.getElementById('ageForm');
-const dayInput = document.getElementById('day').value;
-const monthInput = document.getElementById('month').value;
-const yearInput = document.getElementById('year').value;
+let dayInput = document.getElementById('day');
+let monthInput = document.getElementById('month');
+let yearInput = document.getElementById('year');
 const labels = document.querySelectorAll('label');
 const inputs = document.querySelectorAll('input');
 const smalls = document.querySelectorAll('small');
@@ -9,89 +9,175 @@ const dayOutput = document.getElementById('dayOutput');
 const monthOutput = document.getElementById('monthOutput');
 const yearOutput = document.getElementById('yearOutput');
 
+const yearValue = Number(yearInput.value);
+const monthValue = Number(monthInput.value);
+const dayValue = Number(dayInput.value);
+
 const today = new Date();
-const dayNow = today.getDate();
-const monthNow = today.getMonth() + 1;
-const yearNow = today.getFullYear();
+let dayNow = today.getDate();
+let monthNow = today.getMonth() + 1;
+let yearNow = today.getFullYear();
+
+let days = 0;
+
+function validateYear () {
+    if(yearInput.value === '') {
+        smalls[2].innerText = 'This field is required';
+        labels[2].classList.add('error');
+        inputs[2].classList.add('error');
+        yearOutput.innerText = '--';
+    } else if (yearValue > yearNow) {
+        smalls[2].innerText = 'Must be in the past';
+        labels[2].classList.add('error');
+        inputs[2].classList.add('error');
+        yearOutput.innerText = '--';
+    } else {
+        smalls[1].innerText = '';
+        labels[1].classList.remove('error');
+        inputs[2].classList.remove('error');
+        yearOutput.innerText = yearNow - yearValue;
+        yearOutput.classList.add('success');
+        calculateAge(days);
+    }
+};
+
+ function validateMonth () {
+    if(monthInput.value === '') {
+        smalls[1].innerText = 'This field is required';
+        labels[1].classList.add('error');
+        inputs[1].classList.add('error');
+        monthOutput.innerText = '--';
+    } else if (monthValue > 12){
+        smalls[1].innerText = 'Must be a valid month';
+        labels[1].classList.add('error');
+        inputs[1].classList.add('error');
+        monthOutput.innerText = '--';
+    } else {
+        smalls[1].innerText = '';
+        labels[1].classList.remove('error');
+        inputs[1].classList.remove('error'); 
+        monthOutput.innerText = monthNow - monthValue;
+        monthOutput.classList.add('success');
+        calculateAge(days);
+    }
+};
+
+function validateDay() {
+
+    if(dayInput.value === '') {
+        smalls[0].innerText = 'This field is required';
+        labels[0].classList.add('error');
+        inputs[0].classList.add('error');
+        dayOutput.innerText = '--';
+    } else if(dayValue > 31) {
+        smalls[0].innerText = 'Must be a valid day';
+        labels[0].classList.add('error');
+        inputs[0].classList.add('error');  
+        dayOutput.innerText = '--';     
+    } else {
+        smalls[1].innerText = '';
+        labels[1].classList.remove('error');
+        inputs[0].classList.remove('error');
+        dayOutput.innerText = dayNow - dayValue;
+        dayOutput.classList.add('success');
+        calculateAge(days);
+    }
+
+    switch(monthValue) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10: 
+        case 12: 
+            days = 31;
+                if(dayValue > days) {
+                smalls[0].innerText = 'Must be a valid day';
+                labels[0].classList.add('error');
+                inputs[0].classList.add('error');
+                dayOutput.innerText = '--';
+            } else {               
+                smalls[0].innerText = '';
+                labels[0].classList.remove('error');
+                inputs[0].classList.remove('error');
+                dayOutput.innerText = dayNow - dayValue;
+                dayOutput.classList.add('success');
+                calculateAge(days);
+            }
+        break; 
+        case 2: 
+            if(yearValue % 4 === 0 && yearValue % 100 !== 0 || yearValue % 400 === 0) {
+                days = 29;
+                if(dayValue > days) {
+                    smalls[0].innerText = `February has ${days} days`;
+                    labels[0].classList.add('error');
+                    inputs[0].classList.add('error');
+                    dayOutput.innerText = '--';
+                } else {
+                    smalls[0].innerText = '';
+                    labels[0].classList.remove('error');
+                    inputs[0].classList.remove('error');
+                    dayOutput.innerText = dayNow - dayValue;
+                    dayOutput.classList.add('success');
+                    calculateAge(days);
+                } 
+            } else {
+                days = 28;
+                if(dayValue > days) {
+                    smalls[0].innerText = `February has ${days} days`;
+                    labels[0].classList.add('error');
+                    inputs[0].classList.add('error');
+                    dayOutput.innerText = '--';
+                } else {
+                    smalls[0].innerText = '';
+                    labels[0].classList.remove('error');
+                    inputs[0].classList.remove('error');
+                    dayOutput.innerText = dayNow - dayValue;
+                    dayOutput.classList.add('success');
+                    calculateAge(days);
+                }  
+            }       
+        break;
+        case 4:
+        case 6:
+        case 9:
+        case 11: 
+            days = 30;
+            if(dayValue > days) {
+                smalls[0].innerText = 'Must be a valid day';
+                labels[0].classList.add('error');
+                inputs[0].classList.add('error');
+                dayOutput.innerText = '--';
+            } else {
+                smalls[0].innerText = '';
+                labels[0].classList.remove('error');
+                inputs[0].classList.remove('error');
+                dayOutput.innerText = dayNow - dayValue;
+                dayOutput.classList.add('success');
+                calculateAge(days);
+            }
+        break;
+    };
+};
+
+function calculateAge(days) {
+    if(dayValue > dayNow) {
+        dayNow += days;
+        monthNow--;
+    }
+
+    if(monthValue > monthNow) {
+        monthNow = monthNow + 12;
+        yearNow--;   
+        }
+}
 
 ageForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const yearValue = Number(yearInput);
-    const dayValue = Number(dayInput);
-    const monthValue = Number(monthInput);
-
-    function leapYear(year) {
-        return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
-    }
-
-const result = leapYear(yearValue);
-console.log(result);
-
-    if(dayInput === '' && monthInput === '' && yearInput === '') {
-        labels.forEach(label => {
-            label.classList.add('error');
-        });
-       inputs.forEach(input => {
-        input.classList.add('error');
-       }); 
-       smalls.forEach(small => {
-        small.innerText = 'This field is required';
-       });
-    } else if(dayValue > 31 || monthValue > 12 || yearValue > yearNow) {
-        labels.forEach(label => {
-            label.classList.add('error');
-        });
-        inputs.forEach(input => {
-            input.classList.add('error');
-        });
-       smalls[0].innerText = 'Must be a valid day';
-       smalls[1].innerText = 'Must be a valid month';
-       smalls[2].innerText = 'Must be in the past';
-    } else if(dayValue > 28 && monthValue === 2 && result === false) {
-        labels.forEach(label => {
-            label.classList.add('error');
-        });
-        inputs.forEach(input => {
-            input.classList.add('error');
-        });
-       smalls[0].innerText = 'This month has only 28 days';
-    } else if(dayValue > 30 && monthValue % 2 === 0) {
-        labels.forEach(label => {
-            label.classList.add('error');
-        });
-        inputs.forEach(input => {
-            input.classList.add('error');
-        });
-       smalls[0].innerText = 'This month has only 30 days';
-    } 
-     else {
-        labels.forEach(label => {
-            label.classList.remove('error');
-        });
-        inputs.forEach(input => {
-            input.classList.remove('error');
-            input.value = '';
-        });
-        smalls.forEach(small => {
-            small.innerText = '';
-        }); 
-
-        let yearAge = 0;
-        yearAge = yearNow - yearValue;
-        yearOutput.innerHTML = yearAge;
-        console.log(yearAge);
-        yearOutput.classList.add('success');
-
-        let monthAge = 0;
-        monthAge = yearAge * 12;
-        monthOutput.innerHTML = monthAge;
-        monthOutput.classList.add('success');
-
-        let daysAge = 0; 
-        daysAge = yearAge * 365;
-        dayOutput.innerHTML = daysAge;
-        dayOutput.classList.add('success');  
-    }
+    validateDay();
+    validateMonth();
+    validateYear();
 }); 
 
 
